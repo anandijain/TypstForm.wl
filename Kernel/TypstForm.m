4eb5f5dd782f616ffe52018e92fac8e2e4352960
anand jain <anandj@uchicago.edu>
2025-06-05 14:55:28 -0700

    context fix?

--- Kernel/TypstForm.m
+++ Kernel/TypstForm.m
@@ -4,11 +4,9 @@
 BeginPackage["TypstForm`"]
 
 MakeTypstString::usage =
-"MakeTypstString[expr] returns a Typst-compatible string for expr."
+  "MakeTypstString[expr] returns a Typst-compatible string for expr.";
 
-Begin["Private"]
-
-(* 2. Implement MakeTypstString and any helpers here. For example: *)
+Begin["`Private`"]    (* ← note the backticks here *)
 
 ClearAll[MakeTypstString];
 
@@ -16,50 +14,49 @@ MakeTypstString[n_Integer] := ToString[n];
 MakeTypstString[n_Rational] := "(" <> ToString[Numerator[n]] <> ")/(" <> ToString[Denominator[n]] <> ")";
 MakeTypstString[Pi] := "pi";
 MakeTypstString[s_Symbol] /; StringLength[ToString[s]] == 1 := Module[
-{name = CharacterName[ToString[s], "TeXName"]},
-If[
-StringQ[name] && StringLength[name] > 0,
-ToLowerCase[name],
-ToString[s]
-]
+  {name = CharacterName[ToString[s], "TeXName"]},
+  If[
+    StringQ[name] && StringLength[name] > 0,
+    ToLowerCase[name],
+    ToString[s]
+  ]
 ];
 MakeTypstString[s_Symbol] := SymbolName[s];
 MakeTypstString[Exp[x_]] := "e^(" <> MakeTypstString[x] <> ")";
 MakeTypstString[p_Power] /; p[[1]] === E := Module[{ex = p[[2]]},
-"e^(" <> MakeTypstString[ex] <> ")"
+  "e^(" <> MakeTypstString[ex] <> ")"
 ];
 unaryMap = {
-Sin -> "sin",
-Cos -> "cos",
-Tan -> "tan",
-Log -> "log",
-Sqrt -> "sqrt",
-Abs -> "abs",
-Exp -> "e"
+  Sin -> "sin",
+  Cos -> "cos",
+  Tan -> "tan",
+  Log -> "log",
+  Sqrt -> "sqrt",
+  Abs -> "abs",
+  Exp -> "e"
 };
 Scan[
-Function[{rule},
-Module[{matHead = rule[[1]], typstName = rule[[2]]},
-If[matHead =!= Exp,
-MakeTypstString[matHead[x_]] :=
-typstName <> "(" <> MakeTypstString[x] <> ")"
-]
-]
-],
-unaryMap
+  Function[{rule},
+    Module[{matHead = rule[[1]], typstName = rule[[2]]},
+      If[matHead =!= Exp,
+        MakeTypstString[matHead[x_]] :=
+          typstName <> "(" <> MakeTypstString[x] <> ")"
+      ]
+    ]
+  ],
+  unaryMap
 ];
 MakeTypstString[p_Power] := Module[{base, expo},
-{base, expo} = List @@ p;
-MakeTypstString[base] <> " ^ " <> MakeTypstString[expo]
+  {base, expo} = List @@ p;
+  MakeTypstString[base] <> " ^ " <> MakeTypstString[expo]
 ];
 MakeTypstString[t_Times] :=
-StringRiffle[MakeTypstString /@ List @@ t, " * "];
+  StringRiffle[MakeTypstString /@ List @@ t, " * "];
 MakeTypstString[a_Plus] :=
-StringRiffle[MakeTypstString /@ List @@ a, " + "];
+  StringRiffle[MakeTypstString /@ List @@ a, " + "];
 MakeTypstString[Divide[a_, b_]] :=
-"(" <> MakeTypstString[a] <> ")/(" <> MakeTypstString[b] <> ")";
+  "(" <> MakeTypstString[a] <> ")/(" <> MakeTypstString[b] <> ")";
 MakeTypstString[other_] := ToString[InputForm[other]];
 
-End[] (* Private *)
-
+End[]   (* closes `TypstForm`Private` *)
 EndPackage[]
